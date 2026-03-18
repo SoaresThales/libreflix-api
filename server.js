a8700b3e11c5cce7d83b03d53ba05b0b35ebe2de
Thales Soares <108821550+SoaresThales@users.noreply.github.com>
2026-03-18 15:20:31 -0300

    Add files via upload
    feat: implement file system persistence with database.json

--- server.js
+++ server.js
@@ -1,65 +1,11 @@
 const express = require('express'); //importação
 const cors = require('cors'); //importando segurança
+const fs = require('fs'); // manipulador de arquivos
+const { json } = require('stream/consumers');
 const app = express(); //inicialização
 app.use(cors()); // avisando que o front-end pode pedir aqui
+app.use(express.json());
 const PORT = 3000;
-
-// ==========================================
-// 2. O NOSSO BANCO DE DADOS (O ESTOQUE)
-// ==========================================
-
-const movies = [
-    {
-        id: 1,
-        title: "Steamboat Willie",
-        year: 1928,
-        genre: "Animation",
-        plot: "Mickey Mouse pilots a steamboat and turns everything into musical instruments in his first official appearance.",
-        poster: "https://image.tmdb.org/t/p/original/xEur3v9kMfw4zDueCh4wrUuhW5A.jpg",
-        trailer: "https://www.youtube.com/embed/8LdpvQHg3e0?si=TVuuOCMdWiv2YanF",
-        featured: false
-    },
-    {
-        id: 2,
-        title: "The Kid",
-        year: 1921,
-        genre: "Comedy / Drama",
-        plot: "The Tramp cares for an abandoned child, but events put that relationship in jeopardy.",
-        poster: "https://image.tmdb.org/t/p/original/cvWzCDo3pQyoasvqzRIP7oHK3qn.jpg",
-        trailer: "https://www.youtube.com/embed/9pIyRBDpdAM?si=nlO24g7xSXp6Z8MN",
-        featured: true
-    },
-    {
-        id: 3,
-        title: "Felix in Hollywood",
-        year: 1923,
-        genre: "Animation",
-        plot: "Felix the Cat travels to Hollywood and meets the greatest stars of the silent film era.",
-        poster: "https://image.tmdb.org/t/p/original/3P3ztTCe5jL0Rza8m5ZsUT5NhH7.jpg",
-        trailer: "https://www.youtube.com/embed/7eSE5hzTyIo",
-        featured: false
-    },
-    {
-        id: 4,
-        title: "Minnie the Moocher",
-        year: 1932,
-        genre: "Animation",
-        plot: "Betty Boop runs away from home and ends up in a spooky cave with Cab Calloway.",
-        poster: "https://image.tmdb.org/t/p/original/k6D27wXkz0hufbi9EtEqRXFXbcm.jpg",
-        trailer: "https://www.youtube.com/embed/qFhMuFEtIh4?si=HSH-dyki13YEc6ya",
-        featured: false
-    },
-    {
-        id: 5,
-        title: "A Trip to the Moon",
-        year: 1902,
-        genre: "Sci-Fi",
-        plot: "A group of astronomers travel to the Moon in a cannon-propelled capsule and explore its surface.",
-        poster: "https://image.tmdb.org/t/p/original/acBq3tmTcBAHCjbH7Wi5nt8jnfA.jpg",
-        trailer: "https://www.youtube.com/embed/qcyZ70tO9-Q",
-        featured: false
-    }
-]
 
 // ==========================================
 // ROTAS (Os Pedidos)
@@ -70,8 +16,24 @@ app.get('/', function(req,res){
 });
 
 app.get('/api/movies', function(req, res){
+    const data = fs.readFileSync(__dirname + '/database.json', 'utf-8'); // node vai ler aqui agora
+    const movies = JSON.parse(data); // transforma de volta em texto
     res.json(movies);
 });
+
+app.post('/api/movies', function(req, res) {
+    const data = fs.readFileSync(__dirname + '/database.json', 'utf-8');
+    const movies = JSON.parse(data);
+    const newMovie = req.body;
+    const lastMovie = movies[movies.length - 1];
+    newMovie.id = lastMovie ? lastMovie.id + 1 : 1;
+    movies.push(newMovie); // adiciona filmes novos na memória temporária
+    fs.writeFileSync(__dirname + '/database.json', JSON.stringify(movies, null, 2)); // o "null, 2" serve apenas para formatar bonitinho com quebras de linha e recuos
+    res.status(201).json({
+        message: "Filme adicionado com sucesso!",
+        movie: newMovie
+    });
+})
 
 // ==========================================
 // A IGNIÇÃO DO SERVIDOR
